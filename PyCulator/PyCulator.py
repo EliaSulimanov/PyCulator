f51b11b0b07779e6103d82b693917bad4fb6f800
Elia Sulimanov <eliasulimanov@gmail.com>
2019-04-21 21:15:00 +0300

    added the math operations

--- PyCulator/PyCulator.py
+++ PyCulator/PyCulator.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import wpf
 
 from System.Windows import Application, Window
@@ -10,11 +11,16 @@ class MyWindow(Window):
         wpf.LoadComponent(self, 'PyCulator.xaml')
         pass
 
+    def Is_Last_Numeric(text):
+        if(text[-1].isdigit()):
+            return True
+        return False
+
     def Number_Click(self, sender, e):
         self.bigText = self.FindName('Big_Text')
         number_value = getattr(sender, 'Content', 'default')
         global clear
-        if clear:
+        if (clear or self.bigText.Content == '0'):
             clear = False
             self.bigText.Content = number_value
         else:
@@ -23,10 +29,12 @@ class MyWindow(Window):
 
     def Plus_Minus_Click(self, sender, e):
         self.bigText = self.FindName('Big_Text')
+        global clear
         try:
-            num = int(self.bigText.Content)
+            num = float(self.bigText.Content)
             self.bigText.Content = str(-1 * num)
         except:
+            clear = True
             self.bigText.Content = "INVALID"
         pass
 
@@ -35,7 +43,7 @@ class MyWindow(Window):
         self.smallText = self.FindName('Small_Text')
         clear_value = getattr(sender, 'Content', 'default')
         global clear
-        if (clear_value == 'C'):
+        if (clear_value == 'CE'):
             clear = True
             self.bigText.Content = '0'
         else:
@@ -48,6 +56,77 @@ class MyWindow(Window):
         self.bigText = self.FindName('Big_Text')
         new_value = self.bigText.Content[:-1]
         self.bigText.Content = new_value
+        pass
+
+    def Dot_Click(self, sender, e):
+        self.bigText = self.FindName('Big_Text')
+        global clear
+        if clear:
+            clear = False
+            self.bigText.Content = '0.'
+        else:
+            if (self.bigText.Content.find('.') == -1):
+                self.bigText.Content += '.'
+        pass
+
+    def Basic_Operation_Click(self, sender, e):
+        self.bigText = self.FindName('Big_Text')
+        self.smallText = self.FindName('Small_Text')
+        operation_value = getattr(sender, 'Content', '+')
+        if(operation_value == 'X'):
+            operation_value = '*'
+        global clear
+        
+        if (self.bigText.Content != 'INVALID'):
+            if (self.smallText.Content == '0' or self.smallText.Content == '' or self.smallText.Content == 'Error'):
+                if(self.bigText.Content.find('Error') != -1):
+                    pass
+                else:
+                    self.smallText.Content = self.bigText.Content + operation_value
+            else:
+                try:
+                        self.smallText.Content += self.bigText.Content + operation_value
+                except:
+                    self.smallText.Content = 'Error'
+            try:
+                self.bigText.Content = str(eval(self.smallText.Content))
+            except:
+                try:
+                    self.bigText.Content = str(eval(self.smallText.Content + '0'))
+                except:
+                    self.bigText.Content = 'Math Error'
+            clear = True
+        pass
+
+    def Diva_Click(self, sender, e):
+        self.bigText = self.FindName('Big_Text')
+        self.smallText = self.FindName('Small_Text')
+        global clear
+        if (self.bigText.Content != 'INVALID'):
+            if (self.smallText.Content == '0'):
+                self.smallText.Content = self.bigText.Content + '/'
+            else:
+                self.smallText.Content += self.bigText.Content + '/'
+            try:
+                num1 = float(eval(self.smallText.Content))
+                num2 = float(self.bigText.Content)
+                self.bigText.Content = str(num1 / num2)
+            except:
+                self.bigText.Content = '0'
+            clear = True
+        pass
+
+    def Equ_Click(self, sender, e):
+        self.bigText = self.FindName('Big_Text')
+        self.smallText = self.FindName('Small_Text')
+        global clear
+        if (self.bigText.Content != 'INVALID'):
+            try:
+                self.bigText.Content = str(eval(self.smallText.Content + self.bigText.Content))
+            except:
+                pass
+            self.smallText.Content = ''
+            clear = True
         pass
     
 if __name__ == '__main__':
